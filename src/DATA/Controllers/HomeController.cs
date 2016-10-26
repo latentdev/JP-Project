1664448765430d821f0b623044dac205825ee32e
Nicholas Olson <latentdev@gmail.com>
2016-10-26 04:22:27 -0700

    1.01
    semi-functional twitter search. currently a stream is opened and catches
    all tweets that contain the query. The problem with this is it only
    catches the tweets none of the background info about the tweet is
    caught. This function would be more of use in a seperate app that builds
    a database of all tweets that we can query.

--- src/DATA/Controllers/HomeController.cs
+++ src/DATA/Controllers/HomeController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 using Microsoft.AspNetCore.Mvc;
+using Tweetinvi;
 
 namespace DATA.Controllers
 {
@@ -40,7 +43,52 @@ namespace DATA.Controllers
         public JsonResult Search(string search)
         {
 
-            return Json(new { foo = search });
+            /*string sURL = "https://api.twitter.com/1.1/search/tweets.json" + "?q=" + WebUtility.UrlEncode(search);
+            WebRequest request;
+            request = WebRequest.Create(sURL);
+            Stream objStream;
+            objStream = request.GetResponse().GetResponseStream();
+
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new System.Uri(sURL));
+            request.BeginGetResponse(new AsyncCallback(ReadWebRequestCallback), request);
+            request.EndGetResponse(new IAsyncResult());*/
+            string tweets =null;
+            bool searching = true;
+            Auth.SetUserCredentials("6AJKMgHTk5L0qUZX1tumVrcf6", "Z6LQQdlzH7vQca56wRdTaLaokvrORtWKUzYovwLLKXDwnGh9O2", "23288297-GKQtuIQzm8xEXGrpocnYiqn2HGMLE84BqWvcYH11n", "r1KgCO7P3TxkhJKzmkwbUPCCWMsy9GYKf1EMMXFUA0h1t");
+            var stream = Tweetinvi.Stream.CreateFilteredStream();
+            stream.AddTrack(search);
+            stream.MatchingTweetReceived += (sender, args) =>
+            {
+               tweets = tweets + "A tweet containing "+search+" has been found; the tweet is '" + args.Tweet + "'";
+            };
+            stream.StreamStopped += (sender, args) =>
+            {
+                var exceptionThatCausedTheStreamToStop = args.Exception;
+                var twitterDisconnectMessage = args.DisconnectMessage;
+                searching = false;
+            };
+            stream.LimitReached += (sender, args) =>
+            {
+                searching = false;
+            };
+            while (searching)
+            {
+                stream.StartStreamMatchingAllConditions();
+            }
+            return Json(new { foo = tweets });
+        }
+
+        private void ReadWebRequestCallback(IAsyncResult callbackResult)
+        {
+            HttpWebRequest myRequest = (HttpWebRequest)callbackResult.AsyncState;
+            using (HttpWebResponse myResponse = (HttpWebResponse)myRequest.EndGetResponse(callbackResult))
+            {
+                using (StreamReader httpwebStreamReader = new StreamReader(myResponse.GetResponseStream()))
+                {
+                    string results = httpwebStreamReader.ReadToEnd();
+                 
+                }
+            }
         }
     }
 }
